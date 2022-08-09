1d0ff424e1aefeacee69a4ca98d863b6d11da820
0_L1 <92831976+ALWEB2@users.noreply.github.com>
2022-08-09 14:47:32 -0400

    Update V3Loader.lua

--- V3Loader.lua
+++ V3Loader.lua
@@ -1,3 +1,2 @@
 -- loader
 loadstring(game:HttpGet('https://raw.githubusercontent.com/ALWEB2/robloxhacks/main/laggerfinal.lua'))()
-loadstring(game:HttpGet('https://raw.githubusercontent.com/ALWEB2/robloxhacks/main/playercount.lua'))()
